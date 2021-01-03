730d8a4bc3ea7cd490f0666d4a7871eed2fb4c7b
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-01-03 13:45:27 +0800

    added index method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
+
   def show
     #@article is an instance variable that can be used in show.html.erb under articles
     # params attribute is the same as the params attribute in javascript. getting the :id that is the same as the identifier in DB
     @article = Article.find(params[:id])
   end
+
+  def index
+    #index uses GET action to show all articles
+    #rails convention
+    @articles = Article.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  #you may check using rails routes --expanded
   #resources :articles #this gives all routes for articles
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show] #this limits the routes 
+  resources :articles, only: [:show, :index] #this limits the routes 
   
 end
