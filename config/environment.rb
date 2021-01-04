23170694f19a50fe181922c8ce821a261fd61874
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-01-04 16:28:22 +0800

    added bootstrap to style the blog

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# To remove the field_with_errors functionality, add the following code to the bottom of the config/environment.rb file:
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
