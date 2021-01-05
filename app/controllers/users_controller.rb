a8d7aace349f106916afed29b9642e93b390257b
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-01-05 14:34:03 +0800

    added update functionality for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -13,9 +14,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Account updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
-    
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
