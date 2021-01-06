3e0b9ef9f946c45d63922e6d12177b1adaba8698
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-01-06 14:20:25 +0800

    added admin functionality in migration file, controllers, and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show, :destroy]
-  before_action :require_user, except: [:show, :index]
+  before_action :require_user, except: [:show, :index, :new, :create]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
     @users = User.paginate(page: params[:page], per_page: 1)
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       #log in user upon saving
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       flash[:notice] = "Registered successfully"
       redirect_to articles_path
     else
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
     #delete user
     @user.destroy
     #set session id to nil since user is still logged in
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user #only set session user id to nil if user is the one deleting his own account (not the admin)
     #notice
     flash[:notice] = "Account and all associated articles successfully deleted"
     #redirect
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You can only edit or delete your own profile"
       redirect_to users_path
     end
